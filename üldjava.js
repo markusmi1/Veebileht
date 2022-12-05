5357ddbdf913cc1ad12e6888a5169e8650d0bb76
Markus Michelis <markusmi@ut.ee>
2022-12-05 20:02:43 +0200

    kolmasleht täiesti valmis!

--- üldjava.js
+++ üldjava.js
@@ -1,13 +1,14 @@
-script
-    window.onscroll = function() {myFunction()};
-    
-    var navbar = document.getElementById("navbar");
-    var sticky = navbar.offsetTop;
-    
-    function myFunction() {
-      if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-      } else {
-        navbar.classList.remove("sticky");
-      }
-    }
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+//kood lingilt: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
